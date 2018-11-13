13b6a2a3f5da3b1299e463235c301800b47611bd
Sergio Flores Genis <sf.cancun@gmail.com>
2018-11-13 15:36:58 -0600

    Correccion de NPM

---
+++ resources/assets/js/components/EmpleadosList.vue
@@ -0,0 +1,20 @@
+<template>
+    <table class="table table-condensed table-striped">
+        <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "EmpleadosList"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
 @extends('layout.main')
+
+@section('content')
+    <div class="container">
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Empleados</h4>
+            </div>
+            <empleados-list></empleados-list>
+        </div>
+
+    </div>
+
+@endsection
